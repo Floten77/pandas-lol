335a7ec96e31d8a5f4a0a27555b9b975862353bd
Florent NGOR <fngor@opnf.fr>
2024-11-04 23:44:58 +0100

    commit with function call to api riot

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # pandas-lol
 Goal is to practice communication with API, pandas, python and Tableau
+
+Based on riot games developer website, there is a package called Data Dragon which centralize every information that is related of champions,runes, items, spells,etc...
+https://developer.riotgames.com/docs/lol#data-dragon
+
--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import requests
+import pandas as pd
+import json
+
+def get_list_champions(version):
+    url = f"https://ddragon.leagueoflegends.com/cdn/{version}/data/en_US/champion.json"
+    response = requests.request("get",url)
+    file = open(f"list_champions/champions_{version}.json","x")
+    file.write(response.text)
+    file.close()
 
 def call_riot_games_api():
     code_api = "xxx"
-    url = "https://europe.api.riotgames.com/riot/account/v1/accounts"
-    authentification = requests.auth.HTTPBasicAuth("apikey",code_api)
-    headers_content  = {'Accept': 'application/json'}
-    response = requests.request("get",url,headers=headers_content,auth=authentification)
-    print(response)
+    url = "https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations"
+    headers_content  = {'X-Riot-Token': f'{code_api}',"Accept": "application/json"}
+    response = requests.request("get",url,headers=headers_content)
+    content = response.text
 
 if __name__ == "__main__":
     print("Bonjour")
-    call_riot_games_api()
+    with open('list_champions/champions_12.1.1.json','r') as f :
+        data = json.load(f)
+    df = pd.DataFrame(data["data"])
+    print(df)
