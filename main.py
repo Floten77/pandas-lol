9fae11c4277576bd5575c83e7476411eb59601e8
Floten77 <florentngor3@gmail.com>
2024-11-05 17:28:07 +0100

    getting name of each id champions in a pandas dataframe

--- main.py
+++ main.py
@@ -15,10 +15,36 @@ def call_riot_games_api():
     headers_content  = {'X-Riot-Token': f'{code_api}',"Accept": "application/json"}
     response = requests.request("get",url,headers=headers_content)
     content = response.text
+    return content
 
 if __name__ == "__main__":
     print("Bonjour")
-    with open('list_champions/champions_12.1.1.json','r') as f :
+    with open('list_champions/champions_14.21.1.json','r') as f :
         data = json.load(f)
-    df = pd.DataFrame(data["data"])
-    print(df)
+    list_id = []
+    list_champions = []
+    for champions in data["data"]:
+        list_id.append(data["data"][champions]["key"])
+    df = pd.DataFrame([
+        {
+            'id': data["data"][champion]["key"],
+            'name': data["data"][champion]["name"],
+            'title': data["data"][champion]["title"],
+            'blurb': data["data"][champion]["blurb"],
+            'info': data["data"][champion]['info'],
+            'tags': data["data"][champion]["tags"],
+            'partype': data["data"][champion]["partype"],
+            'stats': data["data"][champion]["stats"],
+        }
+        for champion in data["data"]
+    ],index = list_id)
+    rotation_champions =json.loads(call_riot_games_api())
+    free_champions = rotation_champions["freeChampionIds"]
+    free_champions_for_newplayers = rotation_champions["freeChampionIdsForNewPlayers"]
+    df.loc[:,'rotation_champions'] = False
+    for champions in free_champions:
+        df.loc[f'{champions}','rotation_champions'] = True
+    df['id'] = df['id'].astype(int)
+    df_rotationchampions = df[df['rotation_champions'] == True].sort_values('id')
+    print(df_rotationchampions)
+   
