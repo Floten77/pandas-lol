4d57c1987b698e99a91b162ddf9c18e9c9ba1d36
Floten77 <florentngor3@gmail.com>
2024-11-06 17:35:02 +0100

    optimizing treating of data

--- main.py
+++ main.py
@@ -10,41 +10,43 @@ def get_list_champions(version):
     file.close()
 
 def call_riot_games_api():
-    code_api = "xxx"
+    code_api = "RGAPI-1a3021be-f146-4656-8b01-d210d8f552b2"
     url = "https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations"
     headers_content  = {'X-Riot-Token': f'{code_api}',"Accept": "application/json"}
     response = requests.request("get",url,headers=headers_content)
     content = response.text
     return content
 
-if __name__ == "__main__":
-    print("Bonjour")
-    with open('list_champions/champions_14.21.1.json','r') as f :
-        data = json.load(f)
-    list_id = []
-    list_champions = []
-    for champions in data["data"]:
-        list_id.append(data["data"][champions]["key"])
-    df = pd.DataFrame([
-        {
-            'id': data["data"][champion]["key"],
-            'name': data["data"][champion]["name"],
-            'title': data["data"][champion]["title"],
-            'blurb': data["data"][champion]["blurb"],
-            'info': data["data"][champion]['info'],
-            'tags': data["data"][champion]["tags"],
-            'partype': data["data"][champion]["partype"],
-            'stats': data["data"][champion]["stats"],
-        }
-        for champion in data["data"]
-    ],index = list_id)
+
+def get_rotation_champions(df):
     rotation_champions =json.loads(call_riot_games_api())
     free_champions = rotation_champions["freeChampionIds"]
     free_champions_for_newplayers = rotation_champions["freeChampionIdsForNewPlayers"]
     df.loc[:,'rotation_champions'] = False
     for champions in free_champions:
         df.loc[f'{champions}','rotation_champions'] = True
-    df['id'] = df['id'].astype(int)
     df_rotationchampions = df[df['rotation_champions'] == True].sort_values('id')
     print(df_rotationchampions)
+
+if __name__ == "__main__":
+    print("Bonjour")
+    version = '14.21.1'
+    with open(f'list_champions/champions_{version}.json','r') as f :
+        data = json.load(f)
+    list_id = []
+    list_champions = []
+    df = pd.DataFrame()
+    for champions in data["data"]:
+        list_id.append(data["data"][champions]["key"])
+        df = df.append(pd.json_normalize(data["data"][champions]))
+    df = df.filter(regex='^(?!image)',axis=1)
+    df = df.drop(['id','version'],axis=1)
+    df = df.rename(columns={"key":"id"})
+    df = df.set_index('id')
+    print(df.info())
+    df.to_csv(f'data_changes/champions_{version}.csv',index = True)
+    # Getting champions which are given for free during the current period 
+    # get_rotation_champions(df)
+
+
    
