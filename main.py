ca29b4182de199102a7c8c7a03b5db035da1c575
Floten77 <florentngor3@gmail.com>
2024-11-14 17:58:17 +0100

    duplicate doesn't existe anymore, but have to remove duplicate couting

--- main.py
+++ main.py
@@ -55,7 +55,6 @@ def get_list_items(version):
 def get_count_items(items_name,df):
     total = 0
     df_parcours = df.loc[df["name"] == f"{items_name}",["name","from"]]
-    print(df_parcours)
     if df_parcours["from"].notna().any():
         total+=get_count_items_intermediary(items_name,df)
     else:
@@ -69,7 +68,7 @@ def get_count_items_intermediary(items_name,df):
     if df_parcours["from"].notna().any():
         for list_items in df_parcours["from"]:
             for items in list_items:
-                total+=get_count_items_intermediary(df.loc[f'{items}',"name"],df)
+                total+=get_count_items_intermediary(df.loc[int(items),"name"],df)
             total += 1
     else:
         return 1
@@ -90,7 +89,10 @@ def get_count_items_into_intermediary(items_name,df):
     if df_parcours["into"].notna().any():
         for list_items in df_parcours["into"]:
             for items in list_items:
-                total+=get_count_items_into_intermediary(df.loc[f'{items}',"name"],df)
+                if(not(int(items) in df.index)):
+                    total += 1
+                    continue
+                total+=get_count_items_into_intermediary(df.loc[int(items),"name"],df)
             total += 1
     else:
         return 1
@@ -115,20 +117,28 @@ def treating_data_items(version):
     df = df.transpose()
     df.index = df.index.str.strip()
     df["name"] = df["name"].str.strip()
+    df.index = df.index.astype('int64')
     df = df.sort_index(ascending = True)
-    print(df)
+    print(df[df['name'] == "Faerie Charm"])
+    print(df.loc[1004])
     #df = (df.sort_values(by='plaintext', ascending=False)
     #    .drop_duplicates(subset=['name'])
     #    )
     df_duplicated = df.duplicated(subset="name",keep="first")
-    print(df_duplicated[df_duplicated.index == "223005"])
-    print(df_duplicated[df_duplicated.index == "3005"])
-    print("3005" > "223005")
+    df = df[~df_duplicated]
+    #print("3005" > "223005")
     #df = get_details_gold_info_price_items(df)
     #count_items_bloodthirster = get_count_items("Trinity Force",df)
     #count_items_boots = get_count_items("Boots",df)
     
+    #print(count_items_bloodthirster)
+    #print(count_items_boots)
+    
+    count_into_items_magic = get_count_into_items("Null-Magic Mantle",df)
+    print(count_into_items_magic)
+
     #df.loc[:,"numberitemsinrecipe"] = 0 
+    #df.loc[:,"numbersubitems"] = 0
     #for row in df.itertuples(index=True):
     #    df.loc[row.Index,"numberitemsinrecipe"] = get_count_items(row.name,df)
     #    df.loc[row.Index,"numbersubitems"] = get_count_into_items(row.name,df)
